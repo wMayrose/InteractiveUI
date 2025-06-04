3edd2f2ac1deb6b409297af990fbb5a44784463e
Willa Mayrose <francesmay775@gmail.com>
2025-06-04 14:45:40 -0500

    button completed

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
+        VStack (){
+            Text("What is your name?")
+            .font(.title)
+            TextField("Type Your Name Here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.blue, width: 1)
+            Button("Submit Name") {
+                
+            } // end button
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        } // end VStack
+        .padding()
     } // end body
 } // end struct
 
